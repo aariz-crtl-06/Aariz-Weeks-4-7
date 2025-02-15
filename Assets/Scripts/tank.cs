5f5b16d08606db66973599c3b48398d10f43f488
aariz-crtl-06 <aarizza75@gmail.com>
2025-02-15 14:05:54 -0500

    Made tank move & made barrel follow mouse

---
+++ Assets/Scripts/tank.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tank : MonoBehaviour
+{
+    public float speed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 pos = transform.position;
+
+        pos.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        pos.y += Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.position = pos;
+    }
+}
