bc0bd2cc221d004f9da5cd4139c7e63ebc04a626
aariz-crtl-06 <aarizza75@gmail.com>
2025-02-15 16:52:39 -0500

    Made bullets fly in direction of barrel!

--- Assets/Scripts/bullets.cs
+++ Assets/Scripts/bullets.cs
@@ -5,22 +5,28 @@ using UnityEngine;
 public class bullets : MonoBehaviour
 {
     public float speed = 5f;
+    public Vector2 direction;
 
     public SpriteRenderer spriteRenderer;
     void Start()
     {
-        
+        Vector3 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouse.z = 0;
+        direction = mouse - transform.position;
+
+        if (mouse.z < 90)
+        {
+            transform.right = direction;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 pos = transform.position;
-        pos.x += speed * Time.deltaTime;
+        pos += speed * direction * Time.deltaTime;
         transform.position = pos;
 
-        Vector3 rot = transform.eulerAngles;
-        //rot.z += 1;
-        transform.eulerAngles = rot;
+        
     }
 }
