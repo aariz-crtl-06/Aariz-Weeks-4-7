56a8b99f621d01c3440e3af897cbb12707f424ca
aariz-crtl-06 <aarizza75@gmail.com>
2025-02-16 16:41:07 -0500

    Trying to add player movement.

--- Assets/assignment 2/scripts/player_movement.cs
+++ Assets/assignment 2/scripts/player_movement.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class player_movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 3;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        Vector2 player = transform.position;
+
+        player.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        player.y += Input.GetAxis("Vertical") * speed * Time.deltaTime;
     }
 }
