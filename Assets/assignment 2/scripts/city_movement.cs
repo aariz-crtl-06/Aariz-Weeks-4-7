5037e68a829b1f96c00bd1c99d5adab00c65c33c
aariz-crtl-06 <aarizza75@gmail.com>
2025-02-16 16:32:52 -0500

    Made the background loop to give movement effect.

--- Assets/assignment 2/scripts/city_movement.cs
+++ Assets/assignment 2/scripts/city_movement.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class city_movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float speed = 4f;
+    
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
+        Vector2 city = transform.position;
         
+
+        if (city.x < -25)
+        {
+           
+            city.x =+ 28;
+            city.x -= speed * Time.deltaTime;
+            transform.position = city;
+        }
+        else
+        {
+            city.x -= speed * Time.deltaTime;
+            transform.position = city;
+        }
     }
 }
